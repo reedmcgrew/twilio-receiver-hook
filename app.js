3dee37e849fc91ac876d58bc006b9580fa6f5a4d
Reed McGrew <reed@familylink.com>
2012-07-01 17:10:28 +0000

    Separated out routes, hook configuration, and webserver configuration from the app.js file.

--- app.js
+++ app.js
@@ -1,45 +1,17 @@
-// Set-up Hook
-var settings = require('./settings')
-  , hookio = require('hook.io');
-var twilioReceiverHook = hookio.createHook({
-  "name": "twilioreceiverhook",
-});
-
-// Set-up Web Server
-var express = require('express');
-var webserver = module.exports = express.createServer();
-webserver.configure(function(){
-  webserver.use(express.bodyParser());
-  webserver.use(express.methodOverride());
-  webserver.use(webserver.router);
-  webserver.use(express.static(__dirname + '/public'));
-});
-webserver.configure('development', function(){
-  webserver.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-webserver.configure('production', function(){
-  webserver.use(express.errorHandler());
-});
+// Module Dependencies
+var settings = require('./settings');
+var twilioReceiverHook = require('./hook');
+var webserver = require('./webserver');
+var routes = require('./routes');
 
 // Routes
-var createReceiver = function(hook){
-    return function(req,res){
-        var payload = req.query;
-        console.log("Received request:");
-        console.log(payload);
-        hook.emit("recvSms",{message:payload.Body,from:payload.From});
-        res.send(200);
-    };
-};
-
-var receiverController = createReceiver(twilioReceiverHook);
+var receiverController = routes.createReceiver(twilioReceiverHook);
 webserver.get('/', receiverController);
 
-
-// Start hook and server
+// Combine hook and server
 twilioReceiverHook.on('hook::ready', function(){
     webserver.listen(settings.port, function(){
-        console.log("Webserver listening on %d in %s mode", webserver.address().port, webserver.settings.env);
+        console.log("Listening for HTTP on %d.", webserver.address().port, webserver.settings.env);
     });
 });
 twilioReceiverHook.start();
